a1733255692129e95e649fdc9a7f36decd46d1b8
Addison Farley <farley.addison@student.greenriver.edu>
2024-01-18 12:17:28 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); //static method
+
+//define a default route
+$f3->route('GET /', function () {
+    //display view page
+    echo "Pet Home";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//run fat-free
+$f3->run(); //instance method
